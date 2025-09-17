874d07071ae94dd69dac9d5a3a75c33b52499f7a
psam-717 <155116265+psam-717@users.noreply.github.com>
2025-09-17 23:34:44 +0000

    added a pressable for navigation

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,23 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Pressable, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { router } from 'expo-router'
 
 const App = () => {
   return (
     <View style={styles.container}>
-      <Text>Barber shop home screen</Text>
+      <Text style={{color: 'white'}}>Barber shop home screens</Text>
+
+      <View style={{flexDirection: 'row'}}>
+        <Text style={{marginHorizontal: 10, color: 'white'}}>Already have an account? </Text>
+        <Pressable
+          onPress={() => {
+            router.push('onboarding')
+          }}
+        >
+          <Text style={{color: 'gold', }}>Sign in</Text>
+        </Pressable>
+      </View>
+
     </View>
   )
 }
@@ -16,6 +29,6 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
-        
+        backgroundColor: "black"
     }
 })
