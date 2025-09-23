751e1b059d30771f6871a16f03e5a2bce283a73c
psam-717 <155116265+psam-717@users.noreply.github.com>
2025-09-23 02:49:23 +0000

    main entry point of application

--- app/index.tsx
+++ app/index.tsx
@@ -1,23 +1,33 @@
-import { Pressable, StyleSheet, Text, View } from 'react-native'
+import { ImageBackground, Pressable, Text,StyleSheet,View, Dimensions } from 'react-native'
 import React from 'react'
-import { router } from 'expo-router'
+import { router } from 'expo-router';
+import {FontAwesome6} from "@expo/vector-icons";
+
+const {width, height} = Dimensions.get('window');
 
 const App = () => {
   return (
     <View style={styles.container}>
-      <Text style={{color: 'white'}}>Barber shop home screens</Text>
+      <ImageBackground
+        source={require('../assets/images/background.jpeg')}
+        style={styles.backgroundImage}
+      >
 
-      <View style={{flexDirection: 'row'}}>
-        <Text style={{marginHorizontal: 10, color: 'white'}}>Already have an account? </Text>
-        <Pressable
-          onPress={() => {
-            router.push('onboarding')
-          }}
-        >
-          <Text style={{color: 'gold', }}>Sign in</Text>
+        <View style={{marginTop: height* 0.65, marginLeft: 20, width: width * 0.5}}>
+          <Text style={{color: 'white', fontSize: 35}}>LOCATE THE NEAREST BARBERSHOP AROUND YOU</Text>
+        </View>
+
+        <Pressable 
+          style={{backgroundColor: 'yellow', width: 100, height: 100, alignSelf: 'flex-end', borderTopLeftRadius: 30, borderBottomRightRadius: 30, marginRight: 10, alignItems:'center', justifyContent:'center'}}
+          onPress={() => router.push('auth/signIn')}
+          >
+          <FontAwesome6 name='scissors' size={30} color='black'/>
+
         </Pressable>
-      </View>
 
+
+
+      </ImageBackground>
     </View>
   )
 }
@@ -27,8 +37,12 @@ export default App
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: "black"
+        backgroundColor: "black",
+        
+    },
+    backgroundImage: {
+      width: width,
+      height: height,
+      resizeMode: 'cover'
     }
 })
