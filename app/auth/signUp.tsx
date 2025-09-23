74a62f2f0e66e6db5d46e476f31d4a4faeddd5b4
psam-717 <155116265+psam-717@users.noreply.github.com>
2025-09-23 02:44:26 +0000

    set up the sign up screen

---
+++ app/auth/signUp.tsx
@@ -0,0 +1,20 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const SignUp = () => {
+  return (
+    <View style={styles.container}>
+      <Text>SignUp</Text>
+    </View>
+  )
+}
+
+export default SignUp
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignContent: 'center',
+        alignItems: 'center'
+    }
+})
